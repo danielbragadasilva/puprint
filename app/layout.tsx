816d751c655cb0b79b762c2f81904244e81b3c76
danielbragadasilva <danyelsgs@gmail.com>
2025-03-28 14:53:58 -0300

    update adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,8 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3426508365113232"
+     crossorigin="anonymous"></script>
     </html>
   );
 }
